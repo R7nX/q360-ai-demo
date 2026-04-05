1384de6b6ff1513e4438778c42fd37543b1e54fd
R7nX <baokhanh0514@gmail.com>
2026-04-04 23:19:58 -0600

    temp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["http://10.10.10.50:3000"],
 };
 
 export default nextConfig;
